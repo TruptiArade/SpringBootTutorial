1ea781c3c0b8d4672928c0b9541a9bbbaab45951
TruptiArade <truptiarade1414@gmail.com>
2024-02-07 14:22:48 +0530

    This is a simple spring boot application using REST API

---
+++ BookRestExample/src/main/java/com/test/BookRestExampleApplication.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRestExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRestExampleApplication.class, args);
+	}
+
+}
