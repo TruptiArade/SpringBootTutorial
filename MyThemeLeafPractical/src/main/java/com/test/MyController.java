59e895bc2a9fa5681ad663759f70287a27679ead
TruptiArade <truptiarade1414@gmail.com>
2024-02-13 14:30:40 +0530

    Arithmetic operation program

--- MyThemeLeafPractical/src/main/java/com/test/MyController.java
+++ MyThemeLeafPractical/src/main/java/com/test/MyController.java
@@ -1,6 +1,9 @@
 package com.test;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +14,14 @@ public class MyController {
 	
 	
 	@RequestMapping(value="/about", method=RequestMethod.GET)
-	public String about()
+	public String about(Model model)
 	{
+		
+		//Putting Data in model
+		model.addAttribute("name","Trupti Arade");
+		model.addAttribute("currentDate",new Date().toLocaleString());
 		return "about";
+		
 		
 	}
 }
